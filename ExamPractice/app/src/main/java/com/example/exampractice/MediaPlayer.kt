512d4fd4eb406f9838cd057953eff7cbc44d5077
Abdul Manaf <sahitoa97@gmail.com>
2022-10-25 21:43:48 +0500

    Media Player for Audio Sound

--- ExamPractice/app/src/main/java/com/example/exampractice/MediaPlayer.kt
+++ ExamPractice/app/src/main/java/com/example/exampractice/MediaPlayer.kt
@@ -1,11 +1,33 @@
 package com.example.exampractice
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MediaPlayer : AppCompatActivity() {
+    private lateinit var audio : MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media_player)
+        audio = MediaPlayer.create(applicationContext, R.raw.love)
+    }
+    fun playSong(view : View){
+        if (!audio.isPlaying) {
+            audio.start()
+        }
+    }
+    fun pauseSong(view : View){
+        if (audio.isPlaying){
+            audio.pause()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (audio.isPlaying){
+            audio.stop()
+        }
     }
 }
