fdf9e74554b212221f14f39bfe3ae7e86e8b2628
Abdul Manaf <sahitoa97@gmail.com>
2022-10-25 23:51:23 +0500

    SQLite Database added

--- ExamPractice/app/src/main/java/com/example/exampractice/SQLiteDatabaseExample.kt
+++ ExamPractice/app/src/main/java/com/example/exampractice/SQLiteDatabaseExample.kt
@@ -1,11 +1,50 @@
 package com.example.exampractice
 
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class SQLiteDatabaseExample : AppCompatActivity() {
+
+    private lateinit var database : SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sqlite_database_example)
+        database = openOrCreateDatabase("Manaf", MODE_PRIVATE, null)
+//        createTable()
+//        insertValues(1, "Abdul Manaf", "03003141736")
+        showData()
+    }
+
+    private fun createTable() {
+        if (database.isOpen){
+            val createTable : String = "CREATE TABLE Student(id INTEGER, name TEXT, contact TEXT);"
+            database.execSQL(createTable)
+            Toast.makeText(this, "Successfully created Table ... !", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun insertValues(id : Int, name : String, contact : String){
+        if (database.isOpen){
+            val insertValue : String = "INSERT INTO Student values(${id}, ${name}, ${contact});"
+            database.execSQL(insertValue)
+            Toast.makeText(this, "Successfully Values inserted ... !", Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun showData(){
+        val showCommand : String = "SELECT * FROM Student"
+        val cr = database.rawQuery(showCommand, null)
+        Toast.makeText(this, cr.count, Toast.LENGTH_LONG).show()
+        if (cr.isFirst && cr.moveToFirst()) {
+            do
+            {
+                val id = cr.getInt(0)
+                val name = cr.getString(1)
+                Toast.makeText(this, "ID: ${id} : Name: ${name}", Toast.LENGTH_LONG).show()
+            }while (cr.moveToNext())
+            cr.close()
+        }
     }
 }
